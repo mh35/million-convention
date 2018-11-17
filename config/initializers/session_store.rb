dbbbc090c70c6da541dc663e483e8b3b44c8d2c9
honda <mh35jp@gmail.com>
2018-11-17 17:41:22 +0900

    Session fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 Rails.application.config.session_store :redis_store,
   servers: Rails.application.config.session_store_servers,
-  expire_in: MillionConvention::Application.config.session_expires_in
+  expire_in: 30.minutes
