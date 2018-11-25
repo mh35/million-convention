e987eda4cb9aaf8f0c6f303e986148e83c7dba19
mh35 <mh35jp@gmail.com>
2018-11-25 14:16:57 +0900

    Login implement and test login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'net/http'
 require 'uri'
 
 class LoginController < ApplicationController
@@ -34,5 +36,40 @@ class LoginController < ApplicationController
       return
     end
     # After this, get request token
+    api_http = Net::HTTP.new('api.line.me', 443)
+    api_http.use_ssl = true
+    token_resp = nil
+    api_http.start do
+      token_resp = api_http.post_form('/oauth2/v2.1/token', {
+        'grant_type' => 'authorization_code',
+        'code' => params['code'],
+        'redirect_uri' =>
+          'https://million-convention.mh35.info/login/callback',
+        'client_id' => ENV['LINE_OPENID_CHANNEL_ID'],
+        'client_secret' => ENV['LINE_OPENID_CHANNEL_SECRET']
+      })
+    end
+    unless token_resp.code.to_i < 300
+      flash[:error_msg] = 'ログイン処理でエラーが発生しました'
+      redirect_to controller: 'top', action: 'index'
+      return
+    end
+    token_body = JSON.parse(token_resp.body)
+    access_token = token_body['access_token']
+    expire_time = Time.now + token_body['expires_in']
+    refresh_token = token_body['refresh_token']
+    id_token = token_body['id_token']
+    decoded_token = JWT.decode(id_token, ENV[
+      'LINE_OPENID_CHANNEL_SECRET'], true, {algorithm: 'HS256'})
+    decoded_id_token = decoded_token[0]
+    unless decoded_id_token['iss'] == 'https://access.line.me' &&
+      decoded_id_token['aud'] == ENV['LINE_OPENID_CHANNEL_ID'] &&
+      decoded_id_token['nonce'] == session[:login_nonce]
+      flash[:error_msg] = '不正なIDトークンです'
+      redirect_to controller: 'top', action: 'index'
+      return
+    end
+    flash[:error_msg] = decoded_id_token['name']
+    redirect_to controller: 'top', action: 'index'
   end
 end
