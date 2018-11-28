e8a87bf9c706201390f064084acb2646ecded6e8
honda <honda@10-kai.jp>
2018-11-28 12:43:42 +0900

    Create login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
   def prepare_default_title
     @title = 'コンベンションセンター'
   end
+
+  def require_admin
+    unless session[:is_admin]
+      redirect_to controller: 'top', action: 'index'
+    end
+  end
+
+  def require_login
+    unless session[:uid]
+      redirect_to controller: 'top', action: 'index'
+    end
+  end
 end
