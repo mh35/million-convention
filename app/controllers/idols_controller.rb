6feb2f2cee8f6d2d1a884d1c988db575e9b67f06
honda <honda@10-kai.jp>
2018-11-28 13:04:30 +0900

    Idol create controller

---
+++ app/controllers/idols_controller.rb
@@ -0,0 +1,46 @@
+class IdolsController < ApplicationController
+  before_action :require_admin, except: [:show]
+  def show
+    begin
+      @idol = Idol.find(params[:id])
+    rescue
+      redirect_to controller: 'top', action: 'index'
+    end
+  end
+
+  def new
+    @idol = Idol.new
+  end
+
+  def create
+    @idol = Idol.new(params[:idol])
+    @idol.save
+    redirect_to action: 'show', id: @idol.id
+  end
+
+  def edit
+    begin
+      @idol = Idol.find(params[:id])
+    rescue
+      redirect_to controller: 'top', action: 'index'
+    end
+  end
+
+  def update
+    begin
+      @idol = Idol.find(params[:id])
+    rescue
+      redirect_to controller: 'top', action: 'index'
+    end
+  end
+
+  def destroy
+    begin
+      @idol = Idol.find(params[:id])
+    rescue
+      redirect_to controller: 'top', action: 'index'
+    end
+    @idol.destroy
+    redirect_to controller: 'top', action: 'index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/login/callback', to: 'login#callback'
   get '/logout', to: 'login#logout'
   get '/privacy', to: 'privacy#index'
+  resources :idols, except: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
