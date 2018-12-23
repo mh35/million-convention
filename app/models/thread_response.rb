e775d6162e8c45eceedd684028afff2f6718b016
honda <mh35jp@gmail.com>
2018-12-23 18:21:15 +0900

    ActiveRecord relationships

--- app/models/thread_response.rb
+++ app/models/thread_response.rb
@@ -1,4 +1,5 @@
 class ThreadResponse < ApplicationRecord
   belongs_to :idol_thread
   belongs_to :user
+  has_many :reports
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :thread_responses
+  has_many :reports
 end
